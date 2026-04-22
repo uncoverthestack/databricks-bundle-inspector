a08a2a80319b612842f21e18e1900130cfbdeac9
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2026-04-22 22:25:37 +0100

    refactor: add uppercase for case insensitivity

--- src/test/fixtures/secret-scope-example/src/query.sql
+++ src/test/fixtures/secret-scope-example/src/query.sql
@@ -1,4 +1,4 @@
 SELECT
     secret('jdbc-test', 'db-host')  AS db_host,
-    secret('jdbc-test', 'db-user')  AS db_user,
+    SECRET('jdbc-test', 'db-user')  AS db_user,
     try_secret('jdbc-test', 'db-password') AS db_password
