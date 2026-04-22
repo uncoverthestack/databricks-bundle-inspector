cc3180e5a48f9979df73e20729ed014e5daded1d
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2026-04-22 15:24:53 +0100

    fix imports

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,3 +1,5 @@
+import { jest, beforeEach } from "@jest/globals";
+
 beforeEach(() => {
   jest.spyOn(console, "log").mockImplementation(() => {});
   jest.spyOn(console, "warn").mockImplementation(() => {});
