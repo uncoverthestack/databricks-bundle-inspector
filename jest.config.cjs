463fe7a6ec160daaab1ec67fc78f7fea1cf88d6c
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2026-04-22 11:30:47 +0100

    refactor: fix warning with ts-jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,16 +6,16 @@ const tsJestPreset = createDefaultPreset();
 module.exports = {
   testEnvironment: "node",
   transform: {
-    ...tsJestPreset.transform,
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
   },
   testMatch: ["<rootDir>/src/test/unit/**/*.test.ts"],
   clearMocks: true,
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.jest.json",
-    },
   },
 };
